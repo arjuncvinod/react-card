78a45f5ac24946d5bf61eaf8e63264a91e2614d2
arjuncvinod <arjuncvinod007@gmail.com>
2023-07-01 01:05:57 +0530

    added avatar component

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function avatar(props) {
+  return <img className="circle-img" src={props.id} alt="avatar_img" />;
+}
+export default avatar;
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,17 +1,19 @@
-function card(props){
-    return (
-      <div>
-        <div className="card">
-          <div className="top">
-            <h2 className="name">{props.name}</h2>
-            <img className="circle-img" src={props.img} alt="avatar_img" />
-          </div>
-          <div className="bottom">
-            <p className="info">{props.tel}</p>
-            <p className="info">{props.email}</p>
-          </div>
+import React from "react";
+import Avatar from "./Avatar";
+function card(props) {
+  return (
+    <div>
+      <div className="card">
+        <div className="top">
+          <h2 className="name">{props.name}</h2>
+          <Avatar id={props.img}/>
+        </div>
+        <div className="bottom">
+          <p className="info">{props.tel}</p>
+          <p className="info">{props.email}</p>
         </div>
       </div>
-    );
+    </div>
+  );
 }
-export default card
+export default card;
