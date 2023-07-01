d834f3cdb4ce7f0933fc7c63d59e16be85d28ae6
arjuncvinod <arjuncvinod007@gmail.com>
2023-07-01 23:45:48 +0530

    index.js -u

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 //1. Apply CSS styles to App.jsx component
 //to match the appearance on the completed app:
-//https://c6fkx.csb.app/
 //2. Extract the contact card as a reusable Card component.
 //3. Use props to render the default Beyonce contact card
 //so the Card component can be reused for other contacts.
